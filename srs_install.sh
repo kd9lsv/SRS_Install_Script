7d74e64b26f8aea55d14450c61c154a607be6ebc
Connor KD9LSV <connor@kd9lsv.me>
2024-12-21 11:22:48 -0600

    Remove Extra Script Files and build in NR

--- srs_install.sh
+++ srs_install.sh
@@ -12,32 +12,15 @@ echo "Are you wanting to update the Dashboard? Note this will not delete your da
 read -p "(Y/n) " dashboard_update
 
 if  [[ $first_install != 'n' ]] && [[ $first_install != 'N' ]];then
-# Download script python files.
-mkdir ~/bin
-cd ~/bin
-curl -o shutdown_button.py -fsSL https://www.packtenna.com/uploads/1/2/2/7/122774721/shutdown_button.py.txt 
-curl -o getip.py -fsSL https://www.packtenna.com/uploads/1/2/2/7/122774721/getip.py.txt
-# Control Serial Interface
+
+## Control Serial Interface
 sudo raspi-config nonint do_serial_hw 0
 sudo raspi-config nonint do_serial_cons 1
 
-# Create shutdown service.
-echo "[Unit]
-Description=SRS_Shutdown
-After=multi-user.target
-
-[Service]
-User=$USER
-ExecStart=/usr/bin/python3 ~/bin/shutdown_button.py
-
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/srs_shutdown.service
-sudo chmod +x /etc/systemd/system/srs_shutdown.service
-sudo systemctl daemon-reload
-sudo systemctl enable srs_shutdown.service
-
 # Install minicom not needed?
-sudo apt-get install minicom -y -qq > /dev/null
+sudo apt-get install git python3-rpi.gpio minicom -y -qq > /dev/null
+echo "Install Git and python Y"
+wait
 fi
 
 if  [[ $flag_update != 'n' ]] && [[ $flag_update != 'N' ]]; then
@@ -57,17 +40,14 @@ echo "**NodeRed Dashboard Status**"
 echo "Updating and Upgrading your Pi to newest standards  Y"
 echo "Install and Update NodeRed  Y"
 fi
-# -- Install git & Python -- #
-sudo apt-get install git python3-rpi.gpio  -y -qq > /dev/null
-echo "Install Git and python Y"
-wait
 
+# -- Install NR Flow and Nodes -- #
 echo "**The next step will take around 5 minutes. Please be patient.**"
 echo  -n "Install modules for SRS Dashboard."
 cd $HOME/.node-red
 if [[ $dashboard_update != 'n' ]] && [[ $dashboard_update != 'N' ]]; then
 curl -o flows.json -fsSL https://www.packtenna.com/uploads/1/2/2/7/122774721/flows__1_.json
-npm --prefix ~/.node-red/ install node-red node-red-contrib-email node-red-contrib-string node-red-contrib-ui-led node-red-dashboard node-red-node-serialport node-red-contrib-python3-function @js-on/node-red-contrib-py3run node-red-contrib-simple-gate node-red-node-pi-gpio node-red-contrib-buffer-parser --silent
+npm --prefix ~/.node-red/ install node-red node-red-contrib-email node-red-contrib-string node-red-contrib-ui-led node-red-dashboard node-red-node-serialport node-red-contrib-simple-gate node-red-node-pi-gpio node-red-contrib-buffer-parser @ralphwetzel/node-red-systeminformation node-red-contrib-unit-converter node-red-smooth node-red-contrib-moment --silent
 fi
 echo "  Y"
 wait
